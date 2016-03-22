83e0a62512101d9bb6882c3f621de0bda7a92ebb
Qian <Qian@QiandeMacBook-Air.local>
2016-03-22 11:34:28 +0800

    modify 88. Merge Sorted Array

--- _88_merge_sorted_array.c
+++ _88_merge_sorted_array.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 
+/* 使用附加数组
+
 void merge(int* nums1, int m, int* nums2, int n) {
 	int i=0,j=0,k=0;
 	int temp[m+n];
@@ -21,7 +23,23 @@ void merge(int* nums1, int m, int* nums2, int n) {
 	for(i=0;i<m+n;i++){
 		nums1[i]=temp[i];
 	}
+}*/
 
+void merge(int* nums1, int m, int* nums2, int n) {
+	int i=m-1,j=n-1,k=m+n-1;
+	while(i>=0 && j>=0){
+		if(nums1[i]>=nums2[j]){
+			nums1[k--]=nums1[i--];
+		}else{
+			nums1[k--]=nums2[j--];
+		}
+	}
+	while(i>=0){
+		nums1[k--]=nums1[i--];
+	}
+	while(j>=0){
+		nums1[k--]=nums2[j--];
+	}
 }
 
 int main(){
